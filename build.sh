e87c7bb8ebcbf17f59c5162643247fb2546d1dba
Josh Cheek <josh.cheek@gmail.com>
2017-02-05 07:47:22 -0600

    Add CodeMirror for editing, Opal evaluates the code when it changes

--- build.sh
+++ build.sh
@@ -2,10 +2,16 @@
 set -eu
 set -o pipefail
 
-# We'll assume you already have Ruby and Rubygems
+# We'll assume you already have Ruby/Rubygems and Node/Npm
 
+# Ruby dependencies
 which bundler >/dev/null ||
   gem install bundler
 
+# Compile Opal for in-browser Ruby
 bundle install &&
   bundle exec rake compile
+
+# Compile JavaScript stuff
+npm install &&
+  npm run bower
