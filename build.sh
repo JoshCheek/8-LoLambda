3323d9786ce026ab8eb5df0199afdbf6e205e63d
Josh Cheek <josh.cheek@gmail.com>
2017-02-10 22:19:56 -0600

    Remove toplevel implementation (Opal/Bower/etc)

--- build.sh
+++ build.sh
@@ -2,16 +2,5 @@
 set -eu
 set -o pipefail
 
-# We'll assume you already have Ruby/Rubygems and Node/Npm
-
-# Ruby dependencies
-which bundler >/dev/null ||
-  gem install bundler
-
-# Compile Opal for in-browser Ruby
-bundle install &&
-  bundle exec rake compile
-
-# Compile JavaScript stuff
-npm install &&
-  npm run bower
+# Assumes Node/Npm are already installed
+npm install && npm start
