92eb45cb966dd7ada166edaf0789e34eb3eeb0d9
Josh Cheek <josh.cheek@gmail.com>
2017-02-12 06:21:40 -0600

    Some more test names

--- src/update_state.test.js
+++ src/update_state.test.js
@@ -11,19 +11,29 @@ const newState = () => build()
 
 describe('Updating the state', () => {
   describe('RUN_TEST event', () => {
+    // does this also need hooks for test results?
+    // eg "UPDATE_TEST_RESULT" for after the test is run?
     // run test (id, body)
+    it('finds the tests for the code block, runs them, and updates their status')
+    it('uses the body it was given in the event')
+    it('TODO: what happens if they timeout?')
+    it('TODO: what about dependencies? eg we want TRUE to be available in the env')
   })
 
   describe('SAVE_RESULT event', () => {
     // save result (id, body)
+    it('copies the code block\'s body into its function in state.functions')
   })
 
   describe('GOTO event', () => {
     // goto (id) // section | component
+    it('sets the currentSection to the section for the id')
+    it('TODO: what about when the id is for a segment? is there a way to jump to that loc? maybe via URL fragment')
   })
 
   describe('EDIT_CODE event', () => {
     // editCode (id, body)
+    it('saves the body into its code segment')
   })
 
   describe('unknown events', () => {
