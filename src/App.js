e728bd152f51a59674f84cad6e8fedffaa15d300
Josh Cheek <josh.cheek@gmail.com>
2017-02-13 06:10:33 -0600

    React app renders the first segment's markdown (as text)

--- src/App.js
+++ src/App.js
@@ -1,5 +1,5 @@
 import React, { Component } from 'react'
-import CodeMirror from 'react-codemirror'
+// import CodeMirror from 'react-codemirror'
 import './App.sass'
 
 import 'codemirror/lib/codemirror.css'
@@ -13,70 +13,70 @@ import 'codemirror/mode/javascript/javascript.js'
 
 class Navbar extends Component {
   render() {
-    return <div className="Navbar">
-      {this.props.pages.map((page, index) => this.buildPageLink(page, index))}
-    </div>
+    const links = this.props.sections.map((sec, index) =>
+      <a className="PageLink" key={index}>
+        {sec.segments[0].body.split("\n")[0]}
+      </a>)
+    return <div className="Navbar">{links}</div>
   }
 
-  buildPageLink(page, index) {
-    return <a className="PageLink" key={index} onClick={() => this.props.setPage(page)}>
-      {page.name}
-    </a>
-  }
+  // buildPageLink(page, index) {
+  //   return <a className="PageLink" key={index} onClick={() => this.props.setPage(page)}>
+  //     {page.name}
+  //   </a>
+  // }
 }
 
 class Page extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      code: `// Code\n(a) => a * a`,
-      options: {
-        lineNumbers: true,
-        autofocus:   true,
-        mode:        "javascript",
-        theme:       'solarized',
-      },
-    }
-  }
-  updateCode(newCode) {
-    this.setState({code: newCode})
-  }
+  // constructor(props) {
+  //   super(props)
+  //   id, segments
+  //   this.state = {
+  //     code: `// Code\n(a) => a * a`,
+  //     options: {
+  //       lineNumbers: true,
+  //       autofocus:   true,
+  //       mode:        "javascript",
+  //       theme:       'solarized',
+  //     },
+  //   }
+  // }
+  // updateCode(newCode) {
+  //   this.setState({code: newCode})
+  // }
   render() {
     const page = this.props.page
+    const segment = page.segments[0]
+    console.log(segment)
     return <div className="Page">
-      <h2>{page.name}</h2>
-      <p>{page.content}</p>
-      <CodeMirror value={this.state.code}
-                  onChange={(code) => this.updateCode(code)}
-                  options={this.state.options}
-      />
+      {segment.body}
     </div>
+      // dangerouslySetInnerHTML={{
+      //   __html:
+      // }} />
+
+    // const page = this.props.page
+    // return <div className="Page">
+    //   <h2>{page.name}</h2>
+    //   <p>{page.content}</p>
+    //   <CodeMirror value={this.state.code}
+    //               onChange={(code) => this.updateCode(code)}
+    //               options={this.state.options}
+    //   />
+    // </div>
   }
 }
 
 class App extends Component {
-  constructor(props) {
-    super(props)
-    this.state = {
-      functions: {
-      },
-      currentPageIndex: 0,
-      pages: [
-        { name: "Name 1", content: JSON.stringify(props.appState) },
-        { name: "Name 2", content: "Second content" },
-      ],
-    }
-  }
-
   render() {
-    const state = this.state
-    const currentPage = state.pages[state.currentPageIndex]
-    const setPage = (page) => {
-      this.setState({currentPageIndex: state.pages.indexOf(page)})
-    }
+    const appState = this.props.appState
+    const crntIdx  = appState.currentSection || 0
+    const current  = appState.sections[crntIdx]
+    console.log(current)
+
     return <div className="App">
-      <Navbar pages={state.pages} setPage={setPage}/>
-      <Page page={currentPage}/>
+      <Navbar sections={appState.sections} current={current} />
+      <Page page={current}/>
     </div>
   }
 }
