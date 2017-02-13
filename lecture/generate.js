faeda6d0700e9deb5d1ca8eac4725a2dd666f00c
Josh Cheek <josh.cheek@gmail.com>
2017-02-13 05:30:36 -0600

    A script to convert the lecture markdown files into JSON

--- lecture/generate.js
+++ lecture/generate.js
@@ -1,14 +1,16 @@
 #!/usr/bin/env babel-node
-
 'use strict'
+
 const fs = require('fs')
 const Build = require('../src/build_state')
+const path = (filename) => `${__dirname}/${filename}`
 
 fs.readdir(__dirname, (err, files) =>
-  console.dir(
-    Build.stateFromMarkdownBodies(
-      files
-        .filter(f => f.match(/\.md$/))
-        .map(f => `${__dirname}/${f}`)
-        .map(f => fs.readFileSync(f).toString())),
-    {colors: true, depth: 20}))
+  fs.writeFileSync(
+    path("lecture.json"),
+    JSON.stringify(
+      Build.stateFromMarkdownBodies(
+        files.filter(f => f.match(/\.md$/))
+             .map(path)
+             .map(f => fs.readFileSync(f))
+             .map(md => md.toString())))))
