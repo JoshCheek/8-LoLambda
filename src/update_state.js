7a0b8f831c365326eb0473c32da12fd8f850d4eb
Josh Cheek <josh.cheek@gmail.com>
2017-02-17 10:11:51 -0600

    Delete some files I wasn't using

--- src/update_state.js
+++
@@ -1,9 +0,0 @@
-'use strict'
-export default function update(state, event, data) {
-  switch(event) {
-    case "RUN_TEST":
-      break
-    default:
-      throw(`Unknown event: ${event}`)
-  }
-}
