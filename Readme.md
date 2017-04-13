15f6d7eff91d2cea0f3eced7c1fe77257ceba465
Josh Cheek <josh.cheek@gmail.com>
2017-04-13 13:17:13 -0500

    Remove the irrelevant opal / ruby stuff

---
+++ Readme.md
@@ -0,0 +1,10 @@
+Edit XML with CodeMirror text editor
+====================================
+
+To use:
+
+```sh
+$ npm install
+$ npm run bower
+$ open index.html
+```
--- opal_app.rb
+++
@@ -1,6 +0,0 @@
-# Not sure why we need this file, but whatever
-# Things seem to be pruned if you don't require them
-# so just use this file to require stuff we want in the browser
-
-# require 'opal'
-require 'opal/parser'
